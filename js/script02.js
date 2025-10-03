6e8ea5c16a3684075595c19c51f83cfdc1abb621
Taynara <taynarasantossousa09@gmail.com>
2025-10-03 20:52:20 -0300

    Add files via upload

---
+++ js/script02.js
@@ -0,0 +1,5 @@
+alert("Programa de subtração")
+var numero01 = Number(prompt("Digite um número:")) 
+var numero02 = parserInt(prompt("Digite outro número:"))
+soma = numero01+numero02 
+alert("A soma dos números digitados é:"+subtracao) 
